1b405d3d64326427cc70f08616c181d05379006d
Lu <eyelenm@outlook.com>
2024-08-07 13:57:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let contador2 = 1   
+while(contador2 <=5){
+    console.log("trola")
+}
+
+
+/* https://onedrive.live.com/view.aspx?resid=%7BF5C49D3D-B4D8-2029-808A-C00400000000%7D&id=documents&wd=target%28Programaci%C3%B3n%20web%20b%C3%A1sica.one%7C415319F4-2554-4A73-B5AC-D751DA68911C%2FClase%2015%20%5C%7C%20%C3%81mbitos%7CA8ACC5E4-AB1B-495C-82C4-C5920C8824A1%2F%29
+onenote:https://d.docs.live.net/8a29b4d8f5c49d3d/Documentos/Anotaciones/Programación%20web%20básica.one#Clase%2015%20|%20Ámbitos&section-id={415319F4-2554-4A73-B5AC-D751DA68911C}&page-id={A8ACC5E4-AB1B-495C-82C4-C5920C8824A1}&end */
